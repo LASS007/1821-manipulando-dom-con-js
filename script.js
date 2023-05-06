91f1ff77b95356c52d0bfaa2ce56bd7b4b72173b
LASS <l-a-s-s@outlook.com>
2023-05-06 16:47:33 -0600

    Icono delete funcionando ya todo dunciona
    Se grego la funcionalidad al boton de basura para elimar las tareas,
    hasta estara el codigo comentado aparentemente en el siguiente commit se
    resstructurara

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // aparentemente para seleccionar un data- se hace dentro de "[]"", para id, class o tag se hace "",
 
-// funcion discreta de esta manera encierra todo el codigo en una funcion indirecta que a la ves se ejecuta inmediatamente ( () => {} ) ();
+// funcion discreta de esta manera encierra todo el codigo en una funcion indirecta que a la ves se ejecuta inmediatamente ( () => {} ) (); a esto se le llama Immediately Invoked Function Expression IIFE funcion que como tal se declaran se ejecutan.
 ( () => {
   
 const btn = document.querySelector("[data-form-btn]");
@@ -25,9 +25,11 @@ const createTask = (evento) => {
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
-    // Recuerde que cuando una funcion se le agrega () se manda a llamar inmediatamente
+    // Recuerde que cuando una funcion se le agrega () se manda a llamar inmediatamente, tambien que el appendChild reacciona en el orden de arriba a abajo, es decir similar a estar en el HTML
     taskContent.appendChild(checkComplete());
+    
     taskContent.appendChild(titleTask);
+    
 
     
     // const content = `<div>
@@ -36,6 +38,8 @@ const createTask = (evento) => {
     // </div><i class="fas fa-trash-alt trashIcon icon"></i>`
     
     task.appendChild(taskContent);
+    // Como i de la seccion card el ultimo i esta fuera del div por eso se declara hasta aca el hijo deleteIcon, recuerde la constante se convierte en funcion indirecta por eso ()
+    task.appendChild(deleteIcon());
     
     // task.innerHTML = content;
 
@@ -76,6 +80,19 @@ const completeTask = (event) => {
 // de la constante btn que agarra los datos de button en html tiene el evento de escuchar cuando se hace un click activa createTask
 btn.addEventListener("click", createTask);
 
+const deleteIcon = () => {
+  const i = document.createElement("i");
+  i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+  i.addEventListener("click", deleteTask);
+  return i;
+}
+
+// de esta manera de accesa al target padre y con la funcion remove se eliman todo el padre que contiene la tarjeta card
+const deleteTask = (event) => {
+  console.log(event.target.parentElement);
+  const parent = event.target.parentElement;
+  parent.remove();
+} 
 // en la configuracion de modo desarrollador del navegador, preserver log, sirve para conservar los .log generados aunque se recargue la pagina por eso del live server ejemplo.
 
 // Todos los elementos en nuestro árbol de DOM son nodos y todos los nodos pueden ser accedidos vía JavaScript. Los nodos pueden ser eliminados, creados o modificados. Durante el curso utilizamos el método appendChild que siempre es implementado al final del nodo, para colocar un nodo hijo dentro del nodo padre.
